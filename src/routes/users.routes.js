1da6d16891150bec36f594585f9f6a79c2ff0a54
fernando <isaacfernando5351@gmail.com>
2023-08-03 16:47:51 -0600

    rutas prueba

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -11,4 +11,20 @@ router.get('/', async(req, res, next) => {
     }
 });
 
+router.post('/', async(req, res, nex)=> {
+    try {
+        console.log(req.body);
+        const query = 'INSERT INTO users (name, lastname, email, password, id_role, created_at) VALUES (?, ?, ?, ?, ?, ?);';
+
+        const result = await sequelize.query(query, {
+            replacements: [req.body.name, req.body.lastname, req.body.email, req.body.password, req.body.idRole, req.body.created_at],
+            type: sequelize.QueryTypes.INSERT,
+        });
+        console.log(result);
+        res.status(201).json(result);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 module.exports = router;
