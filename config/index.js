79487e7659d7dd04e0c13edb50bdf48a1e717d24
fernando <isaacfernando5351@gmail.com>
2023-08-03 16:01:22 -0600

    variables de entorno

--- config/index.js
+++ config/index.js
@@ -1,11 +1,15 @@
 require ('dotenv').config();
 
 const config = {
+    env: process.env.NODE_ENV || 'development',
+    isProd: process.env.NODE_ENV === 'production',
+    port: process.env.PORT || 3000,
     dbName: process.env.DB_NAME,
     db_host: process.env.DB_HOST,
     db_port: process.env.DB_PORT,
     db_user: process.env.DB_USER,
-    db_password: process.env.DB_PASSWORD
+    db_password: process.env.DB_PASSWORD,
+    DB_URI: process.env.DB_STRING
 }
 
 module.exports = config
