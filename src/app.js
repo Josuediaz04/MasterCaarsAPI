37a7e2588ce8cdb313f8d7a82645a49507f0184e
fernando <isaacfernando5351@gmail.com>
2023-08-03 16:04:12 -0600

    agregando routing

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const port = process.env.PORT || 3000
-const app = express()
-const conection = require ('../database/index')
+const app = express();
+const setupRouter = require('./routes');
 
 app.set('port',port)
 app.use(express.json())
 
-
+setupRouter(app);
 
 
 module.exports = app
