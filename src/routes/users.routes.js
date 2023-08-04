d5ee8d467cd2dae7b4d50c292ba00fef67ee15b1
fernando <isaacfernando5351@gmail.com>
2023-08-03 19:24:36 -0600

    routing de users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,30 +1,91 @@
+const UserService = require('../services/user.service');
+
 const router = require('express').Router();
-const sequelize = require('../../libs/sequelize');
 
-router.get('/', async(req, res, next) => {
-    try {
-        const users = await sequelize.query('select * from users;');
-        console.log(users);
-        res.json(users);
-    } catch (error) {
-        console.log(error);
+const service = new UserService;
+
+router.get('/', 
+    async(req, res, next) => {
+        try {
+            const users = await service.readAll();
+            res.status(200).json({
+                statusCode: 200,
+                message: 'users fetched',
+                data: users
+            });
+        } catch (error) {
+            next(error);
+            console.log(error);
+        }
     }
-});
+);
 
-router.post('/', async(req, res, nex)=> {
-    try {
-        console.log(req.body);
-        const query = 'INSERT INTO users (name, lastname, email, password, id_role, created_at) VALUES (?, ?, ?, ?, ?, ?);';
-
-        const result = await sequelize.query(query, {
-            replacements: [req.body.name, req.body.lastname, req.body.email, req.body.password, req.body.idRole, req.body.created_at],
-            type: sequelize.QueryTypes.INSERT,
-        });
-        console.log(result);
-        res.status(201).json(result);
-    } catch (error) {
-        console.log(error);
+router.get('/:id', 
+    async(req, res, next) => {
+        try {
+            const { id } = req.params;
+            const user = await service.readByPk(id);
+            res.status(302).json({
+                statusCode: 302,
+                message: 'user found',
+                data: user
+            });
+        } catch (error) {
+            next(error);
+            console.log(error);
+        }
     }
-})
+);
+
+router.post('/', 
+    async(req, res, next)=> {
+        try {
+            const user = await service.create(req.body);
+            res.status(201).json({
+                statusCode: 201,
+                message: 'user created',
+                data: user
+            });
+        } catch (error) {
+            next(error);
+            console.log(error);
+        }
+    }
+);
+
+router.patch('/', 
+    async(req, res, next)=> {
+        try {
+            const { id } = req.params;
+            const user = await service.update(id, req.body);
+            res.status(202).json({
+                statusCode: 202,
+                message: 'user updated',
+                data: user
+            });
+        } catch (error) {
+            next(error);
+            console.log(error);
+        }
+    }
+);
+
+router.delete('/', 
+    async(req, res, next)=> {
+        try {
+            const { id } = req.params;
+            await service.delete(id);
+            res.status(202).json({
+                statusCode: 202,
+                message: 'user deleted',
+                data: id
+            });
+        } catch (error) {
+            next(error);
+            console.log(error);
+        }
+    }
+);
+
 
 module.exports = router;
