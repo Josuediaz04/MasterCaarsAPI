d929201b1f81efd51e2a7977f6b5d690c5f3ad8a
fernando <isaacfernando5351@gmail.com>
2023-08-04 12:05:34 -0600

    configuracion de passport para strategies

---
+++ src/auth/index.js
@@ -0,0 +1,6 @@
+const passport = require('passport');
+const jwtStrategy = require('./strategies/jwt.strategy');
+const localStrategy = require('./strategies/local.strategy');
+
+passport.use(jwtStrategy);
+passport.use(localStrategy);
